c6c4fee0eefc304c4ce1809d626bbe50882f02d3
eluts <eluts15@gmail.com>
2018-04-11 10:31:13 -0700

    created hotfix.

--- feature_branch.go
+++ feature_branch.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("This is a test run on feature_branch.")
 	fmt.Println("Added another feature branch.")
 	fmt.Println("Created release 0.1.0")
+	fmt.Println("Created a hotfix.")
 }
